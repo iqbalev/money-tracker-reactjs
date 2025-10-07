4a8c40a2fb9cc79bebf06fe839f6c284187e556a
Muhammad Iqbal <52034320+iqbalev@users.noreply.github.com>
2025-10-07 19:25:24 +0700

    Refactor TransactionList component
    - Replace key props using date.getTime() instead of index.
    - Sort transaction list by newest added as default.

--- src/components/TransactionList.tsx
+++ src/components/TransactionList.tsx
@@ -8,30 +8,35 @@ function TransactionList() {
   const { translate } = useTranslation();
 
   return moneyTracker.transactions.length > 0 ? (
-    moneyTracker.transactions.map((tx, idx) => {
-      const { date, time } = formatDateAndTime(tx.date, "id-ID") as DateAndTime;
+    moneyTracker.transactions
+      .sort((a, b) => b.date.getTime() - a.date.getTime())
+      .map((tx) => {
+        const { date, time } = formatDateAndTime(
+          tx.date,
+          "id-ID"
+        ) as DateAndTime;
 
-      return (
-        <li key={idx}>
-          <div className="transaction left">
-            <p className="date">{date}</p>
-            <p className="time">{time}</p>
-          </div>
+        return (
+          <li key={tx.date.getTime()}>
+            <div className="transaction left">
+              <p className="date">{date}</p>
+              <p className="time">{time}</p>
+            </div>
 
-          <div className="transaction right">
-            <p className="category">{tx.category}</p>
-            <p
-              className={`amount ${
-                tx.type === "income" ? "income" : "expense"
-              }`}
-            >
-              {formatCurrency(tx.amount, "id-ID", "IDR")}
-            </p>
-            {tx.note ? <p className="note">{tx.note}</p> : null}
-          </div>
-        </li>
-      );
-    })
+            <div className="transaction right">
+              <p className="category">{tx.category}</p>
+              <p
+                className={`amount ${
+                  tx.type === "income" ? "income" : "expense"
+                }`}
+              >
+                {formatCurrency(tx.amount, "id-ID", "IDR")}
+              </p>
+              {tx.note ? <p className="note">{tx.note}</p> : null}
+            </div>
+          </li>
+        );
+      })
   ) : (
     <li>{translate("no-transactions")}</li>
   );
